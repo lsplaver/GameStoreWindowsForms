56e6c40c3810085ecd2c227da60b8b5d33ba5afa
Lawrence Splaver <lsplaver263@gmail.com>
2022-11-09 16:05:07 -0500

    Updated the constructors for Customer.cs and Product.cs.

--- GameStore/Customer.cs
+++ GameStore/Customer.cs
@@ -8,8 +8,12 @@ namespace GameStore
 {
     public class Customer
     {
+        public Customer() { }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+
+        public Customer(string firstName, string lastName, string email) { FirstName = firstName; LastName = lastName; Email = email; }
     }
 }
--- GameStore/Product.cs
+++ GameStore/Product.cs
@@ -2,8 +2,12 @@
 {
     public class Product
     {
+        public Product() { }
+
         public string Code { get; set; }
         public string Name { get; set; }
         public string Price { get; set; }
+
+        public Product(string code, string name, string price) {Code = code; Name = name; Price = price; }
     }
 }
